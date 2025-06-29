7ce82b8075b8f759fe6de5fd8fefa0fd2e2c4b82
繆迦徽棠 <miaojazephyris@gmail.com>
2025-06-30 05:31:21 +0800

    protect.js

--- assets/protect.js
+++ assets/protect.js
@@ -1,1 +1,15 @@
+// 禁止右鍵選單
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
 
+// 禁止選字（備援）
+document.addEventListener("selectstart", function (e) {
+  e.preventDefault();
+});
+
+// 禁止複製
+document.addEventListener("copy", function (e) {
+  e.preventDefault();
+  alert("蜉蝣軍密報禁止傳閱");
+});
